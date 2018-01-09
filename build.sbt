03cbc2b190c35cafdb4eb38b28a4ef5426c1bc7a
herval <hervalfreire@gmail.com>
2018-01-08 19:24:23 -0800

    wip

--- build.sbt
+++ build.sbt
@@ -8,5 +8,8 @@ scalaVersion := "2.12.4"
 
 mainClass := Some("hervalicious.slackrelay.sample.Bot")
 
+libraryDependencies ++= Seq(
+  "hervalicious" %% "slack-relay" % "0.1",
+)
 
 herokuAppName in Compile := "testslackrelaybot" // change this to match you app name
--- src/main/scala/hervalicious/slackrelay/sample/Bot.scala
+++ src/main/scala/hervalicious/slackrelay/sample/Bot.scala
@@ -1,7 +1,55 @@
 package hervalicious.slackrelay.sample
 
+import com.twitter.util.Await
+import hervalicious.slackrelay.SlackBot
+import hervalicious.slackrelay.api.slashcommands._
+
 object Bot extends App {
 
-  println("running")
+  val clientId = sys.env.get("SLACK_CLIENT_ID")
+  val secret = sys.env.get("SLACK_CLIENT_SECRET")
+  val token = sys.env.get("SLACK_VERIFICATION_TOKEN")
+  val port = sys.env.get("PORT")
 
+  (clientId, secret, token, port) match {
+    case (Some(clientId), Some(secret), Some(token), Some(port)) =>
+
+      val bot = new SlackBot(
+        clientId = clientId,
+        clientSecret = secret,
+        verificationToken = token,
+        port = port.toInt,
+        slashCommands = Seq(
+          Ping
+        )
+      )
+
+      // waits until the server resources are released
+      Await.ready(
+        bot.start()
+      )
+
+    case _ =>
+      throw new IllegalArgumentException("Please specify the following environment variables: SLACK_API_KEY, SLACK_CLIENT_SECRET, SLACK_VERIFICATION_TOKEN")
+
+  }
+}
+
+
+object Ping extends SlashCommandHandler {
+  override val command = "/ping"
+
+  override def handler(command: SlashCommand, responder: SlashCommandResponder) = {
+    for {
+      _ <- responder.send( // send a response...
+        text = s"pong: ${command.text}",
+        respondTo = ResponseContext.InChannel // ...to the channel where the command was invoked
+      )
+
+      _ <- responder.send( // you can respond multiple times!
+        text = "✌️",
+        respondTo = ResponseContext.RequesterOnly // only the requester will see this response
+      )
+    } yield Unit
+  }
 }
